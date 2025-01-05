f39fb0d9826f653d61494914f182eb0af3536d36
Your GitHub Username <ajay.poddar15fea@gmail.com>
2025-01-05 21:33:48 +0530

    added the hero section

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Header from './componants/Header';
+import Hero from './componants/Hero';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
      <Header/>
+     <Hero/>
     </div>
   );
 }

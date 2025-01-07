bff57ede86d8586f62308c4ab8b474bdf48d73f8
ajaypoddar78890 <ajay.poddar15fea@gmail.com>
2025-01-07 17:48:45 +0530

    changes made in header

--- src/componants/Header.jsx
+++ src/componants/Header.jsx
@@ -32,7 +32,7 @@ const Header = () => {
       <nav className="bg-gradient-to-b    from-green-50 to-green-100 border-b-2 border-green-500">
         <div className=" container  flex items-center justify-between mx-auto p-4">
           {/* Logo */}
-          <a href="#" className="flex items-center">
+          <a href="/" className="flex items-center">
             <img
               src="https://flowbite.com/docs/images/logo.svg"
               className="h-8"
